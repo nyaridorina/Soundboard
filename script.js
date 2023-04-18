f787a9fc5349165d0b5674609c4b8b05776260c8
nyaridori <42637093+nyaridorina@users.noreply.github.com>
2023-04-18 11:24:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+// Define functions to play sounds
+function playK1Sound() {
+    var audio = document.getElementById("K1-audio");
+    audio.currentTime = 0;
+    audio.play();
+  }
+  
+  function playK2Sound() {
+    var audio = document.getElementById("K2-audio");
+    audio.currentTime = 0;
+    audio.play();
+  }
+  
+  function playK3Sound() {
+    var audio = document.getElementById("K3-audio");
+    audio.currentTime = 0;
+    audio.play();
+  }
+  
+  function playK4Sound() {
+    var audio = document.getElementById("K4-audio");
+    audio.currentTime = 0;
+    audio.play();
+  }
+  
+  function playK5Sound() {
+    var audio = document.getElementById("K5-audio");
+    audio.currentTime = 0;
+    audio.play();
+  }
+  
+  function playK6Sound() {
+    var audio = document.getElementById("K6-audio");
+    audio.currentTime = 0;
+    audio.play();
+  }
+  
+  function playK7Sound() {
+    var audio = document.getElementById("K7-audio");
+    audio.currentTime = 0;
+    audio.play();
+  }
+  
+  function playK8Sound() {
+    var audio = document.getElementById("K8-audio");
+    audio.currentTime = 0;
+    audio.play();
+  }
+  
+  function playK9Sound() {
+    var audio = document.getElementById("K9-audio");
+    audio.currentTime = 0;
+    audio.play();
+  }
+  
+
+
+// Add event listeners to buttons
+document.getElementById("K1-button").addEventListener("click", playK1Sound);
+document.getElementById("K2-button").addEventListener("click", playK2Sound);
+document.getElementById("K3-button").addEventListener("click", playK3Sound);
+document.getElementById("K4-button").addEventListener("click", playK4Sound);
+document.getElementById("K5-button").addEventListener("click", playK5Sound);
+document.getElementById("K6-button").addEventListener("click", playK6Sound);
+document.getElementById("K7-button").addEventListener("click", playK7Sound);
+document.getElementById("K8-button").addEventListener("click", playK8Sound);
+document.getElementById("K9-button").addEventListener("click", playK9Sound);
